36031d65da5c9ad09aca1d352a041e7aa1a017d4
Fabien Vergniaud <47561386+BugJackBarron@users.noreply.github.com>
2021-09-20 23:21:10 +0200

    Build

--- site/Miscellanees/Pygame/balle.py
+++ site/Miscellanees/Pygame/balle.py
@@ -2,27 +2,27 @@ import pygame
 from random import randint
 
 class Balle :
-        def __init__(self, image, center) :
-            """ Initialisation d'un objet de classe Balle a partir de deux arguments :
-- image est l'adresse relative ou absolue de l'image voulue pour l'objet ;
-- center est un tuple de deux entiers donnant la position du centre de la balle lors de
-sa création."""
-    
-            self. image = pygame.image.load(image).convert_alpha()
-            self.rect = self.image.get_rect()
-            self.rect.center = center
-            self.vitesse = randint(1,5)
+    def __init__(self, image, center) :
+        """ Initialisation d'un objet de classe Balle a partir de deux arguments :
+    - image est l'adresse relative ou absolue de l'image voulue pour l'objet ;
+    - center est un tuple de deux entiers donnant la position du centre de la balle lors de
+    sa création."""
+
+        self. image = pygame.image.load(image).convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.center = center
+        self.vitesse = randint(1,5)
+
+    def affiche(self, fenetre) :
+        fenetre.blit(self.image, self.rect)
         
-        def affiche(self, fenetre) :
-            fenetre.blit(self.image, self.rect)
-            
-        def deplace(self) :
-            self.rect = self.rect.move(0,self.vitesse)
-            
-            
-        def collision(self, targetRect) :
-            if self.rect.colliderect(targetRect) :
-                return True
-            else :
-                return False
+    def deplace(self) :
+        self.rect = self.rect.move(0,self.vitesse)
+        
+        
+    def collision(self, targetRect) :
+        if self.rect.colliderect(targetRect) :
+            return True
+        else :
+            return False
             
