d7f1a1184197e1055e936d2ad2bdb65bba458628
Fabien Vergniaud <47561386+BugJackBarron@users.noreply.github.com>
2021-09-28 08:51:04 +0200

    Update Pendu

--- docs/NSI/Premiere/C01/test_Projet_Pendu.py
+++ docs/NSI/Premiere/C01/test_Projet_Pendu.py
@@ -2,18 +2,88 @@ from random import choice
 
 
 def choixMot(fichier) :
-    listeMots = []
-    with open(fichier,'r') as file :
-        return choice([m for m in file.readlines()])
+    with open(fichier,'r', encoding='utf8') as file :
+        mot = choice([m for m in file.readlines()]).lower().replace('\n', '')        
+    return mot
+
+def formateMot(mot) :
+    motFinal = ''
+    for l in mot :
+        if l in['é', 'è', 'ê'] :
+            motFinal += 'e'
+        elif l in ['à', 'â'] :
+            motFinal += 'a'
+        elif l =='ô' :
+            motFinal += 'o'
+        elif l == 'î' :
+            motFinal += 'i'
+        elif l in ['ù', 'û'] :
+            motFinal += 'u'
+        elif l == 'ç' :
+            motFinal += 'c'
+        elif l not in [chr(97+i) for i in range(26)] :
+            print(l)
+            raise ValueError('not in latin alphabet')
+        else :                           
+            motFinal+=l
+    return motFinal.upper()
     
-def main() :
-    motATrouver = choixMot('listeMots.txt')
+def afficheMot(motATrouver,lettresUtilisees) :
+    for l in motATrouver :
+        if l in lettresUtilisees :
+            print(l+" ", end="")
+        else :
+            print("_ ", end="")
+    
+def compteRestantes(motATrouver,lettresUtilisees) :
+    restantes = 0
+    for l in motATrouver :
+        if l  not in lettresUtilisees :
+            restantes +=1
+    return restantes
+        
+def affichePendu(nbEchecs) :
+    print(f"Nombre d'echecs : {nbEchecs}")
+    
+def demandeJoueurLettre():
+    return input("Lettre :").upper()
+    
+def uneManche() :
+    motATrouver = choixMot('liste_francais.txt')
+    motATrouver = formateMot(motATrouver)    
     lettresUtilisees = ''
+    nbEchecs = 0
     continuer = True
     while continuer :
-        ...
+        lettreProposee = demandeJoueurLettre()
+        if  lettreProposee not in lettresUtilisees :
+            lettresUtilisees += lettreProposee
+            afficheMot(motATrouver, lettresUtilisees)
+            restantes = compteRestantes(motATrouver, lettresUtilisees)
+            if lettreProposee in motATrouver :                
+                if restantes ==0 :
+                    continuer = False
+            else :
+                nbEchecs +=1
+                affichePendu(nbEchecs)
+                if nbEchecs >10 :
+                    continuer = False
+                    print(f"Vous avez perdu, le mot à découvrir était :{motATrouver}")
+        else :
+            print()
+            print("Lettre déjà proposée")
+            
+def main() :
+    while True :
+        uneManche()
+        rep = input("Vouslez-vous rejouer ? (o/n)")
+        if rep.lower() not in ['o', 'oui', 'y', 'yes'] :
+            break
+    print("Au revoir !")
+    
             
 if __name__ == "__main__" :
-    
+    main()
+            
             
             
