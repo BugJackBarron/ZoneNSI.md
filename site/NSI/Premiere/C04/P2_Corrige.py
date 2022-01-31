cd0f2c0619b1f31705fa67a4b4fcf93ef1469e22
Fabien Vergniaud <47561386+BugJackBarron@users.noreply.github.com>
2022-01-31 15:39:52 +0100

    Build

--- site/NSI/Premiere/C04/P2_Corrige.py
+++ site/NSI/Premiere/C04/P2_Corrige.py
@@ -7,20 +7,96 @@ def carre(n) :
         t.append(i**2)
     return t
 
+
+def carreC(n) :
+    return [nb**2 for nb in range(n+1)]
+
+
 def imagesf(deb,fin) :
     t =[]
     for i in range(deb, fin+1) :
         t.append(3*i**2 -2*i+1)
     return t
 
+def imagesfC(deb, fin) :
+    return [ 3*i**2 -2*i+1 for i in range(deb, fin+1) ]
+
 def genereListe(n) :
     t= []
     for i in range(n) :
         t.append(random.randint(1, n**2))
     return t
+
+def genereListeC(n) :
+    return [random.randint(1, n**2) for i in range(n)]
+    
+    
+def insereV1(monTab, val, i) :
+    # Version avec slices
+    t1 = monTab[:i]  # toutes les valeurs jusqu'à i exclu
+    t2 = monTab[i:] # toutes lesvaleurs à partir de l'indice i
+    return t1 + [val] + t2
+
+def insereV2(monTab, val, i) :
+    # version avec parcours et nouveau tableau
+    newTab = []
+    for k in range(len(monTab)) :
+        if k == i : #si on est à la bonne position
+            newTab.append(val) # on insère la valeur donnée
+        newTab.append(monTab[k]) # dans tous les cas on recopie le tableau
+    return newTab
+
+def insereV3(monTab, val, i):
+    # version avec parcours à l'envers et "en place"
+    monTab.append(0)
+    toto = len(monTab)-2
+    while toto >= i :
+        monTab[toto+1] = monTab[toto]
+        toto = toto - 1 # ou toto -= 1 ou toto += -1
+    monTab[i] = val
+    return None
+
+
+def compter(monTab, val) :
+    # Avec un parcours par valeurs
+    nbOcc = 0
+    for v in monTab :
+        if v == val :
+            nbOcc += 1
+    return nbOcc
+
+def compterIndices(monTab, val) :
+    # avec un parcours par indice
+    positions = []
+    for i in range(len(monTab)) :
+        if monTab[i] == val :
+            positions.append(i)
+    return positions
+
+def separer(monTab, val) :
+    # On effectue un parcours par éléments
+    inf = []# en créant deux tableaus pour les valeurs inférieures ou égales à val
+    sup = [] # et un pour les strictement supérieures
+    for v in monTab :
+        if v> val :
+            sup.append(v)
+        else :
+            inf.append(v)
+    return inf, sup # On renvoie les deux tableaux
+
+def plusProche(monTab,val) :
+    plusProche = monTab[0]
+    mini = abs(plusProche - val) 
+    for elem in monTab :
+        if abs(elem - val)< mini:
+            plusProche = elem
+            mini = abs(plusProche - val) 
+    return plusProche
+    
+            
+    
     
 
-
-t = (12, 14, 15, 16, 17 ,19)
- 
+t = [12, 14, 15, 16, 17 ,19]
+print(insereV1(t, 5, 4))
  
---
+++ site/NSI/Terminale/Devoirs/BB2022/creationBDD.sql
@@ -0,0 +1,30 @@
+/*INSERT INTO Sandwich (nom, prix) VALUES
+("Supreme Wookie", 350),
+("Double Jawa", 280),
+("Cheese Ewok", 120),
+("Jedi's Delight", 300);*/
+/*INSERT INTO Client(nom, adresse, planete, hyperphone) VALUES
+("Dark Vador", "Etoile Noire", "Alderaan", "0666 6666 66"),
+("Leïa Organa", "Palais Royal", "Alderaan", "0654 3210 98"),
+("Han Solo", "cantina de Chalmun", "Tatooine", "0789 5643 21"),
+("Luke Skywalker", "temple jedi", "Ossus", "06219873 45");*/
+/*INSERT INTO Commande (date, idCl) VALUES 
+("27/01/22",3),
+("27/01/22",1),
+("29/01/22",4),
+("30/01/22", 3),
+("30/01/22", 1),
+("30/01/22", 2),
+("01/02/22", 4),
+("01/02/22", 2);*/
+INSERT INTO Ligne (idCo, idS, nombre) VALUES
+(1,1,1),
+(2,4,2),
+(3,4,2),
+(4,1,1),
+(5,4,3),
+(6,3,3),
+(7,2,1),
+(7,3,4),
+(8,4,1),
+(8,3,2);
