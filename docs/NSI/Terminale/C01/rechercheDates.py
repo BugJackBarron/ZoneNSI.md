6f63267dbe3819d99353b0b5cbd4064871630f1e
Fabien Vergniaud <47561386+BugJackBarron@users.noreply.github.com>
2022-09-06 15:44:57 +0200

    Update rechercheDates.py
    Suppression des levées d'erreurs (trop tôt dans la progression)

--- docs/NSI/Terminale/C01/rechercheDates.py
+++ docs/NSI/Terminale/C01/rechercheDates.py
@@ -1,13 +1,11 @@
 from random import randint
-		
+        
 def genere_groupe() -> list :
     """fonction renvoyant un tableau de 23 nombres aléatoires entre 1 et 365"""
     return [randint(1,365) for _ in range(23)]
 
 def contient_doublon(t : list) -> bool :
     """fonction renvoyant un booléen signalant la présence ou non d'un doublon dans le tableau"""
-    if not(isinstance(t, list) or isinstance(t, tuple)) :
-        raise ValueError('t must be an iterable')
     s = [] # s est un tableau temporaire contenant les valeurs testées
     for data in t :
         if data in s : # si data est déjà dans s, alors c'est un doublon
@@ -15,12 +13,10 @@ def contient_doublon(t : list) -> bool :
         else : # sinon on ajoute data à la liste des valeurs testées.
             s.append(data)
     return False
-	
+    
 def teste_hypothese(n : int) -> int:
     """fonction renvoyant le nombre de groupes contenant un doublon
-	sur un échantillon de n groupes"""
-    if not(isinstance(n, int) and n>=1) :
-        raise ValueError("n must be an integer and greater than 1")
+    sur un échantillon de n groupes"""
     nbDoublons = 0 
     for _ in range(n) :
         t = genere_groupe()
