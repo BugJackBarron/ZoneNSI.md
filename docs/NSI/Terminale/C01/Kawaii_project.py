f1583973651eb181f5f251e8150477c8e803d063
Fabien Vergniaud <47561386+BugJackBarron@users.noreply.github.com>
2022-09-15 17:02:51 +0200

    MAJ KAWAII

--- docs/NSI/Terminale/C01/Kawaii_project.py
+++ docs/NSI/Terminale/C01/Kawaii_project.py
@@ -1,21 +1,37 @@
 import pygame as pg
 from random import randint
 
+CURSOR_LEVEL= 5
+CURSOR_COLOURS = [RED, ORANGE, YELLOW, GREEN, PURPLE ]
+
 
 class HappinessLevel :
-    def __init__(self, startLevel = 2) :
-        pass
+    def __init__(self, start_level = 2) :
+        self.start_level = start_level
+        self.max_level = 5
+
     
 class KawaiiMonster :
     
-    def __init__(self, name : str, image_base :str, happiness_max  : int = 100, happiness_speed : int = 2, upgrade_happiness : int= 100) :
+    def __init__(self, name : str, image_base :list[str], happiness_max  : int = 100, happiness_speed : int = 2, base_value : int =100) :
+        """
+        Méthode constructeur d'un monstre :
+
+        * name : chaine de caractère, nom d'un personnage
+        * image_base : listes de noms fichiers (str) pour affichage
+        * happiness_max : niveau maximum de bonheur du monstre
+        * happinesse_speed : vitesse de décroissance du bonheur
+        * base_value : coût de base d'un monstre/coût d'une augmentation de niveau
+         """
+
         self. name = name
         self.image_base = image_base
-        self.happiness_max= happiness_max
+        self.happiness_max = happiness_max
         self.happiness_speed = happiness_speed
         self.happiness = self.happiness_max
-        self.upgrade_happiness = upgrade_happiness
         self.level = 1
+        self.base_value = base_value
+        self.cursor_happiness = self.happiness % 5
         
     def sad(self) :
         self.happiness -= self.happiness_speed*self.level
@@ -23,11 +39,13 @@ class KawaiiMonster :
         
     def feed(self, value) :
         self.happiness += value
+        if self.happiness> self.happiness_max :
+            self.happiness = self.happiness_max
     
     
     def upgrade(self) :
         self.level += 1
-        self.happiness_max += self.upgrade_happiness
+        self.happiness_max += self.base_value
         self.happiness = self.happiness_max//2
         
         
