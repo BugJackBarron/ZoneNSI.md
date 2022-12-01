798860c0020ed260e1d5278d24ef57ce500ed239
Fabien Vergniaud <47561386+BugJackBarron@users.noreply.github.com>
2022-12-01 22:00:54 +0100

    MAJ SQL SW

--- docs/NSI/Terminale/Projets/SQLStarWars/Tuto_CSV_SQL.py
+++ docs/NSI/Terminale/Projets/SQLStarWars/Tuto_CSV_SQL.py
@@ -1,7 +1,7 @@
 import csv
 import sqlite3 as sql
-
-#Extraction des données CSV
+# 
+# #Extraction des données CSV
 
 def extractData(filename) :
     with open(filename, "r", encoding="utf8") as csvfile :
@@ -57,8 +57,6 @@ for line in dico :
         else :
             query += "'"+line[k]+"', "
     
-    print(query)
-
     curs.execute(query)
 
 # NE pas oublier le commit() (tout ou rien)
@@ -79,7 +77,7 @@ res = tuple(p[0] for p in res.fetchall())
 for p in res :
     # Attention aux guillemets
     req = f"INSERT INTO Planet(name) VALUES ('{p}');"
-    print(req)
+    
     curs.execute(req)
     connexion.commit()
 
@@ -135,19 +133,67 @@ query+="('"+listestarship[-1]+"');"
 curs.execute(query)
 connexion.commit()
 
+## Creation de la table species
 
-### Modification de la table People pour lier aux autres tables
 
-def tableLink(original_table, original_column, target_table, target_column) :
-    curs.execute('ALTER TABLE People ADD planet_id TYPE INTEGER REFERENCES Planet(id);')
+def newTable(table_name, attribute_name, column_from, original_table) :
+    curs.execute(f"DROP TABLE IF EXISTS {table_name};")    
+    curs.execute(f"CREATE TABLE {table_name} (id INTEGER PRIMARY KEY, {attribute_name} TEXT );")
 
-    query = curs.execute("SELECT id, name, homeworld  FROM People")
+    res = curs.execute(f"SELECT DISTINCT({column_from}) FROM {original_table}")
+    liste=set()
+    for f in res.fetchall() :
+        if f[0] != None :
+            lf = f[0].split(",")
+            for f in lf :
+                liste.add(f.strip())
+    liste = list(liste)
+    query = f"INSERT INTO {table_name}({attribute_name}) VALUES "
+    for f in liste[:-1] :
+        query+="('"+f+"'),"
+    query+="('"+liste[-1]+"');"
 
-    for p in query.fetchall() :
-        query2 = curs.execute(f"SELECT id FROM Planet wHERE name='{p[2]}';")
-        curs.execute(f"UPDATE People SET planet_id={query.fetchone()[0]} WHERE id = '{p[0]}';")
-        
-
+    curs.execute(query)
     connexion.commit()
+    
+newTable('Specie', 'name', 'species', 'people')
 
-    curs.execute('ALTER TABLE People DROP COLUMN homeworld;')
+
+### Modification de la table People pour lier à la table planet
+
+def tableLink(original_table, original_column, new_column,target_table, target_column) :
+    curs.execute(f'ALTER TABLE {original_table} ADD {new_column} TYPE INTEGER REFERENCES {target_table}({target_column});')
+    query = curs.execute(f"SELECT id, {original_column } FROM {original_table}")
+    for p in query.fetchall() :
+        if p[1]!= None :
+            query2 = curs.execute(f"SELECT id FROM {target_table} WHERE {target_column}='{p[1]}';")
+            a = query2.fetchone()
+            b = p
+            print(a, " - > ",b)
+            req = f"UPDATE {original_table} SET {new_column}={a[0]} WHERE id = '{b[0]}';"
+            curs.execute(req)
+    connexion.commit()
+    curs.execute(f'ALTER TABLE {original_table} DROP COLUMN {original_column};')
+
+
+tableLink('People', 'homeworld', 'planet_id', 'planet', 'name')
+tableLink('People', 'species', 'specie_id', 'Specie', 'name')
+
+### Modification de la table People pour lier à la table Movie (Ajout d'une table ISIN)
+
+curs.execute("DROP TABLE IF EXISTS IsInMovie")
+curs.execute(f'''CREATE TABLE IsInMovie(
+character_id INTEGER REFERENCES People(id),
+movie_id INTEGER REFERENCES Movie(id),
+PRIMARY KEY(character_id, movie_id));''')
+query = curs.execute(f"SELECT id, films FROM people;")
+for p in query.fetchall() :
+    people_id = p[0]
+    people_films  = [p.strip() for p in p[1].split(",")]
+    for f in people_films :
+        query2 = curs.execute(f"SELECT id FROM Movie WHERE title='{f}';")
+        curs.execute(f"INSERT INTO IsInMovie(character_id, movie_id) VALUES ({people_id},{query2.fetchone()[0]});")
+connexion.commit()
+curs.execute(f'ALTER TABLE people DROP COLUMN films;')
+
+
