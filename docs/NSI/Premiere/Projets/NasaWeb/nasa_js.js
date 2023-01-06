ad681fc6aa858b0bd9d407cfe58a8c6a19965be5
Fabien Vergniaud <47561386+BugJackBarron@users.noreply.github.com>
2023-01-06 16:42:44 +0100

    MAJ projet Nasa

--- docs/NSI/Premiere/Projets/NasaWeb/nasa_js.js
+++ docs/NSI/Premiere/Projets/NasaWeb/nasa_js.js
@@ -1,4 +1,4 @@
-const MY_NASA_KEY = "..."
+const MY_NASA_KEY = "RBGXm5wakyukgagSwBM7Vgb69SiYQMQwVxzlFpYK"
 
 // La variable suivante contiendra la liste de tous les objets récupérés par la requête.
 // c'est un objet de type Array, regardez sur le net pour en connaître les propriétés.
@@ -27,7 +27,7 @@ request_parameters =  new Map()
 // alors la variable imgArray contiendra le résultat de la requếté pour les photos de la NAVCAM 
 // de curiosity au sol 50.
 
-function request_to_nasa(parameters){
+async function request_to_nasa(parameters){
     let url = "https://api.nasa.gov/mars-photos/api/v1/rovers/";
     if ((parameters.has('rover'))&&(['curiosity', 'opportunity', 'spirit'].includes(parameters.get('rover')))){
         url += parameters.get('rover')+"/photos";        
@@ -56,6 +56,56 @@ function request_to_nasa(parameters){
     }
 }
 
+
+async function main(request_parameters){
+    let attente_div = document.getElementById("en_attendant");
+    let contenu_div = document.getElementById("Contenu");
+    
+    attente_div.style.display = 'block';
+    contenu_div.style.display = 'none';
+    
+    await request_to_nasa(request_parameters);
+    
+    attente_div.style.display = 'none';
+    contenu_div.style.display = 'block';
+    
+    
+    }
+
 // VOTRE CODE DOIT ETRE SITUE EN DESSOUS
 
+var actualImage = 0
 
+let previous_button = document.getElementById('previous');
+previous_button.addEventListener('click', previous_img);
+let next_button = document.getElementById('next');
+next_button.addEventListener('click', next_img);
+
+let photo_date=document.getElementById('date');
+photo_date.addEventListener('change', get_parameters)
+
+function get_parameters(){
+
+}
+
+function previous_img(){
+    if (actualImage > 0){ actualImage = actualImage -1;} 
+    document.getElementById("photo").src =  imgArray[actualImage]['img_src'];
+	document.getElementById("photo_info").innerText =  (parseInt(imgArray[actualImage]['id'])- parseInt(imgArray[0]['id']))+ " / " + (parseInt(imgArray[imgArray.length-1]['id'])-parseInt(imgArray[0]['id']));
+}
+
+function next_img(){
+    if (actualImage <imgArray.length-1){ actualImage = actualImage +1;} 
+    document.getElementById("curiosity").src =  imgArray[actualImage]['img_src'];
+	document.getElementById("photo_info").innerText =  (parseInt(imgArray[actualImage]['id'])- parseInt(imgArray[0]['id']))+ " / " + (parseInt(imgArray[imgArray.length-1]['id'])-parseInt(imgArray[0]['id']));
+}
+
+
+request_parameters.set('rover','curiosity');
+request_parameters.set('earth_date','2021-12-01');
+request_parameters.set('camera','NAVCAM');
+main(request_parameters);
+
+let image = imgArray[actualImage];
+document.getElementById("curiosity").src =  image;
+document.getElementById("photo_info").innerText =  (parseInt(imgArray[actualImage]['id'])- parseInt(imgArray[0]['id']))+ " / " + (parseInt(imgArray[imgArray.length-1]['id'])-parseInt(imgArray[0]['id']));
