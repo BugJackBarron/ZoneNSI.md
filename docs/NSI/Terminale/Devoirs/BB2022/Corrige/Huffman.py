496bc7a9f9ca78e978c550c6289ebc9355c5f3d3
Fabien Vergniaud <47561386+BugJackBarron@users.noreply.github.com>
2023-01-26 13:24:14 +0100

    Corrections BB 2022

--- docs/NSI/Terminale/Devoirs/BB2022/Corrige/Huffman.py
+++ docs/NSI/Terminale/Devoirs/BB2022/Corrige/Huffman.py
@@ -74,11 +74,115 @@ def trace_graph(G) :
     
 if __name__ == "__main__" :
     phrase = """
-specialite nsi
+Et aussitôt prenant ses jambes à son cou, il s'enfuit dans la
+direction du camp, avec la vitesse des gens de son pays si
+renommés pour leur agilité; mais, quelle que fût la rapidité de sa
+course, le premier qui avait tiré, ayant eu le temps de recharger
+son arme, lui tira un second coup si bien ajusté, cette fois, que
+la balle traversa son feutre et le fit voler à dix pas de lui.
+
+Cependant, comme d'Artagnan n'avait pas d'autre chapeau, il
+ramassa le sien tout en courant, arriva fort essoufflé et fort
+pâle, dans son logis, s'assit sans rien dire à personne et se mit
+à réfléchir.
+
+Cet événement pouvait avoir trois causes:
+
+La première et la plus naturelle pouvait être une embuscade des
+Rochelois, qui n'eussent pas été fâchés de tuer un des gardes de
+Sa Majesté, d'abord parce que c'était un ennemi de moins, et que
+cet ennemi pouvait avoir une bourse bien garnie dans sa poche.
+
+D'Artagnan prit son chapeau, examina le trou de la balle, et
+secoua la tête. La balle n'était pas une balle de mousquet,
+c'était une balle d'arquebuse; la justesse du coup lui avait déjà
+donné l'idée qu'il avait été tiré par une arme particulière: ce
+n'était donc pas une embuscade militaire, puisque la balle n'était
+pas de calibre.
+
+Ce pouvait être un bon souvenir de M. le cardinal. On se rappelle
+qu'au moment même où il avait, grâce à ce bienheureux rayon de
+soleil, aperçu le canon du fusil, il s'étonnait de la longanimité
+de Son Éminence à son égard.
+
+Mais d'Artagnan secoua la tête. Pour les gens vers lesquels elle
+n'avait qu'à étendre la main, Son Éminence recourait rarement à de
+pareils moyens.
+
+Ce pouvait être une vengeance de Milady.
+
+Ceci, c'était plus probable.
+
+Il chercha inutilement à se rappeler ou les traits ou le costume
+des assassins; il s'était éloigné d'eux si rapidement, qu'il
+n'avait eu le loisir de rien remarquer.
+
+«Ah! mes pauvres amis, murmura d'Artagnan, où êtes-vous? et que
+vous me faites faute!»
+
+D'Artagnan passa une fort mauvaise nuit. Trois ou quatre fois il
+se réveilla en sursaut, se figurant qu'un homme s'approchait de
+son lit pour le poignarder. Cependant le jour parut sans que
+l'obscurité eût amené aucun incident.
+
+Mais d'Artagnan se douta bien que ce qui était différé n'était pas
+perdu.
+
+D'Artagnan resta toute la journée dans son logis; il se donna pour
+excuse, vis-à-vis de lui-même, que le temps était mauvais.
+
+Le surlendemain, à neuf heures, on battit aux champs. Le duc
+d'Orléans visitait les postes. Les gardes coururent aux armes,
+d'Artagnan prit son rang au milieu de ses camarades.
+
+Monsieur passa sur le front de bataille; puis tous les officiers
+supérieurs s'approchèrent de lui pour lui faire leur cour, M. des
+Essarts, le capitaine des gardes, comme les autres.
+
+Au bout d'un instant il parut à d'Artagnan que M. des Essarts lui
+faisait signe de s'approcher de lui: il attendit un nouveau geste
+de son supérieur, craignant de se tromper, mais ce geste s'étant
+renouvelé, il quitta les rangs et s'avança pour prendre l'ordre.
+
+«Monsieur va demander des hommes de bonne volonté pour une mission
+dangereuse, mais qui fera honneur à ceux qui l'auront accomplie,
+et je vous ai fait signe afin que vous vous tinssiez prêt.
+
+-- Merci, mon capitaine!» répondit d'Artagnan, qui ne demandait
+pas mieux que de se distinguer sous les yeux du lieutenant
+général.
+
+En effet, les Rochelois avaient fait une sortie pendant la nuit et
+avaient repris un bastion dont l'armée royaliste s'était emparée
+deux jours auparavant; il s'agissait de pousser une reconnaissance
+perdue pour voir comment l'armée gardait ce bastion.
+
+Effectivement, au bout de quelques instants, Monsieur éleva la
+voix et dit:
+
+«Il me faudrait, pour cette mission, trois ou quatre volontaires
+conduits par un homme sûr.
+
+-- Quant à l'homme sûr, je l'ai sous la main, Monseigneur, dit
+M. des Essarts en montrant d'Artagnan; et quant aux quatre ou cinq
+volontaires, Monseigneur n'a qu'à faire connaître ses intentions,
+et les hommes ne lui manqueront pas.
+
+-- Quatre hommes de bonne volonté pour venir se faire tuer avec
+moi!» dit d'Artagnan en levant son épée.
+
+Deux de ses camarades aux gardes s'élancèrent aussitôt, et deux
+soldats s'étant joints à eux, il se trouva que le nombre demandé
+était suffisant; d'Artagnan refusa donc tous les autres, ne
+voulant pas faire de passe-droit à ceux qui avaient la priorité.
+
+On ignorait si, après la prise du bastion, les Rochelois l'avaient
+évacué ou s'ils y avaient laissé garnison; il fallait donc
+examiner le lieu indiqué d'assez près pour vérifier la chose.
 """
     occurences = get_dict(phrase.replace("\n", ""))
     huffman = construire_arbre_huffman(occurences)
-    trace_graph(huffman)
+    #trace_graph(huffman)
     converter = dict()
     construire_codes(huffman,"", converter)
     compressed = "".join(converter[l] for l in phrase if l!= "\n")
