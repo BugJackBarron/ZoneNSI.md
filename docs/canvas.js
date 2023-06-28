3314e0a07ec2856008ba28108a202fe18c5ce42c
Fabien Vergniaud <47561386+BugJackBarron@users.noreply.github.com>
2023-06-28 13:56:23 +0200

    MAj Index + build

--- docs/canvas.js
+++ docs/canvas.js
@@ -1,45 +1,48 @@
-let canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
+let carroussel = document.getElementById('carroussel');
+carroussel.style.display="flex";
+carroussel.style.flexWrap = "wrap";
 
+DocumentFragment.getElementByClass
 
-window.addEventListener('resize', resizeCanvas, false);
-function resizeCanvas() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+function addImage(source, destination){
+    this.image = new Image();
+    this.image.src = source;
 
-    // Redraw everything after resizing the window
-    drawStuff(); 
-  }
-  resizeCanvas();
-
-
-function drawStuff(){
-
-let cvw = canvas.width;
-thonny =  new Image() ;
-thonny.src = "https://user-images.githubusercontent.com/1057839/104211453-61c0f400-5434-11eb-8f52-c61c616578da.png";
-
-notepadpp = new Image();
-notepadpp.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Notepad%2B%2B_Logo.svg/277px-Notepad%2B%2B_Logo.svg.png"
-
-capytale = new Image();
-capytale.src = "https://capytale2.ac-paris.fr/logo.svg";
-
-
-thonny.onload = function (){
-    ctx.drawImage(thonny, 0, 0, cvw/5, thonny.height/thonny.width*cvw/5);
+    this.dest = destination;
+    let iw = this.image.width;
+    let ih = this.image.height;
+    
+    let link = carroussel.appendChild(document.createElement('a'));
+    let img = link.appendChild(document.createElement('img'));
+    img.src=this.image.src;
+    img.width = 200;
+    img.height = ih*200/iw;
+    link.href = this.dest;
+    link.target="_blank";
+    link.style.display = "block";
+    link.style.width = "2OOpx;"
+    link.style.margin="auto";
+    img.addEventListener('mouseover', ()=>{
+        img.style.border="solid 10px ";
+        img.style.borderColor ="#546d78"
+    }, false);
+    img.addEventListener('mouseout', ()=>{
+        img.style.border="none"
+    }, false);
 }
-notepadpp.onload = function(){
-    ctx.drawImage(notepadpp, cvw/5,0, cvw/5, notepadpp.height/notepadpp.width*cvw/5);
-
-}
-capytale.onload = function(){
-    ctx.drawImage(capytale, 2*cvw/5,0, cvw/5, capytale.height/capytale.width*cvw/5);
-
-}
-}
-
-function drawCan(){
-
-}
-
+//Python
+addImage("https://www.python.org/static/community_logos/python-logo-master-v3-TM.png","https://www.python.org/")
+//Thonny
+addImage("https://user-images.githubusercontent.com/1057839/104211453-61c0f400-5434-11eb-8f52-c61c616578da.png","https://thonny.org/");
+//Capytale
+addImage("https://capytale2.ac-paris.fr/logo.svg","");
+//Pygame
+addImage("https://www.pygame.org/docs/_images/pygame_logo.png","");
+//HTML5
+addImage("https://www.w3.org/html/logo/img/html5-display.png","");
+//CSS3
+addImage("https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/363px-CSS3_logo_and_wordmark.svg.png","");
+//Javascript
+addImage("https://w7.pngwing.com/pngs/503/848/png-transparent-javascript-computer-icons-software-developer-cascading-style-sheets-javascript-logo-angle-text-rectangle-thumbnail.png","");
+//Notepad++
+addImage("https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Notepad%2B%2B_Logo.svg/277px-Notepad%2B%2B_Logo.svg.png","");
--- docs/index.md
+++ docs/index.md
@@ -1,26 +1,15 @@
 # Maths/SNT/NSI dans un petit Lycée Breton
 
 
-<div id="parent_canvas">
-<canvas id = "canvas" width = "500" height = "300">
+<div id="carroussel">
 
-</canvas>
+
 <script src ="canvas.js">
 </script>
 </div>
 
-| Langages | Logiciels | Tutoriels |  |
-| --- | --- | --- | --- |
-| [![Python Logo](https://www.python.org/static/img/python-logo.png){: style="width:50%;vertical-align: middle; align : center;display:table-cell;background-color: #d2dce0;" title="Python Logo"}](https://www.python.org/){: target="_blank"} | [![Thonny](https://user-images.githubusercontent.com/1057839/104211453-61c0f400-5434-11eb-8f52-c61c616578da.png){: style="width:50%; margin:auto;display:block;background-color: #d2dce0;" title="Thonny Logo"}](https://thonny.org/){: target="_blank"}  | r | r |
-
-
+{{ IDEv('concatenate', MAX=3) }}
 
 L'ancien site peut-être trouvé [ici](https://old.zonensi.fr).
-
-
-
-{{ IDE('concatenate', MAX=3) }}
-
-
 
 
